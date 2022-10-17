a57a223eb1b973c3c094790117145eefeceac8ea
Xellyum <105124397+Xellyum@users.noreply.github.com>
2022-10-17 22:02:30 +0200

    engine done

---
+++ src/TextObject.java
@@ -0,0 +1,12 @@
+import javafx.scene.control.Label;
+
+public class TextObject extends Label {
+
+    public TextObject(int x, int y, String text) {
+        setTranslateX(x);
+        setTranslateY(y);
+        setText(text);
+    }
+
+
+}
